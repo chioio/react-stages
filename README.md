c6c81f377fb36d468e531bb8ccf999229946246f
Tenn Chio <Tenn_Chio@icloud.com>
2021-01-07 23:14:14 +0800

    ☺︎ add: jsx syntax

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Stages of React learning
+
+## Basic
+
+### JSX Introducing
+
+Babel会把JSX转译成一个名为 `React.createElement()` 函数的调用。
+
+```jsx
+const element = (
+	<h1 className="greeting">
+    Hello, world!
+  </h1>
+)
+// 等效
+const element = React.createElement(
+	'h1',
+  {className: 'gretting'},
+  'Hello, world!'
+)
+// 实际创建(React元素)
+const element = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello, world!'
+  }
+}
+```
--- packages/basic/index.js
+++ packages/basic/index.js
@@ -2,3 +2,31 @@
 const element1 = <h1>Hello, world!</h1>
 ReactDOM.render(element1, document.getElementById('hello'))
 
+// JSX
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>
+  }
+  return <h1>Hello, Stranger.</h1>
+}
+
+const user = {
+  firstName: 'Tenn',
+  lastName: 'Chio'
+}
+
+const element2 = <div tabIndex="0"></div>
+const element3 = <img src={user.avatarUrl}></img>
+const element4 = (
+  <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here.</h2>
+  </div>
+)
+
+ReactDOM.render(getGreeting(user), document.getElementById('jsx'))
+
